0c7c3d73661175f31484104512f7db5a78e4f6db
Pavel Novitsky <altusensix@gmail.com>
2012-07-27 14:04:19 +0400

    add func tycoon_write

--- tycoonclient.h
+++ tycoonclient.h
@@ -16,6 +16,7 @@ char *magicbuf;
 char buf[BUF_LEN];
 struct sockaddr_in sock_in;
 struct hostent *phe;
+int offset = 0x00;
 
 // timer var
 struct timeval start, end;
@@ -44,7 +45,7 @@ int tycoon_set(int ktsock, char *skey, char *svalue, uint64_t sxt) {
 	uint32_t ksiz = strlen(skey);
 	uint32_t vsiz = strlen(svalue);
 	uint32_t magicbufsize = sizeof(kt_set_magic) + sizeof(flags) + sizeof(rnum) + sizeof(dbidx) + sizeof(ksiz) + sizeof(vsiz) + sizeof(sxt);
-	int offset = 0x00;
+	offset = 0x00;
 
 	flags = htonl(flags);
 	rnum = htonl(rnum);
@@ -82,34 +83,9 @@ int tycoon_set(int ktsock, char *skey, char *svalue, uint64_t sxt) {
         else {
 		write(ktsock, magicbuf, magicbufsize);
 
-		int k;
-		offset = 0x00;
-		for( k=0; k<(strlen(skey)/BUF_LEN)+1; k++) {
-			if( strlen(skey+offset) < BUF_LEN ) {
-				memcpy(buf,skey+offset,strlen(skey+offset));
-				write(ktsock, buf, strlen(skey+offset));
-			}
-			else {
-				memcpy(buf,skey+offset,BUF_LEN);
-				write(ktsock, buf, BUF_LEN);
-			}
-			offset+=BUF_LEN;
-		}
-
-		offset = 0x00;
-                for( k=0; k<(strlen(svalue)/BUF_LEN)+1; k++) {
-			if( strlen(svalue+offset) < BUF_LEN ) {
-                                memcpy(buf,svalue+offset,strlen(svalue+offset));
-				write(ktsock, buf, strlen(svalue+offset));
-                        }
-                        else {
-                                memcpy(buf,svalue+offset,BUF_LEN);
-				write(ktsock, buf, BUF_LEN);
-                        }
-                        offset+=BUF_LEN;
-		}
-
-		free(magicbuf);
+		tycoon_write(ktsock,skey);
+		tycoon_write(ktsock,svalue);
+                free(magicbuf);
 
                 if (read(ktsock, &errbuf, sizeof(errbuf))>0){
                         if ((unsigned char)errbuf[0] != (unsigned char)kt_set_magic) {
@@ -125,8 +101,45 @@ int tycoon_set(int ktsock, char *skey, char *svalue, uint64_t sxt) {
         }
 }
 
-int tycoon_get(unsigned char *gkey) {
-	uint32_t flags = 0x00;
+int tycoon_get(int ktsock, char *gkey) {
+	uint8_t kt_get_magic = 0xBA;
+        uint32_t flags = 0x00;
+        uint32_t rnum = 0x01;
+        uint16_t dbidx = 0x00;
+        uint32_t ksiz = strlen(gkey);
+        uint32_t magicbufsize = sizeof(kt_get_magic) + sizeof(flags) + sizeof(rnum) + sizeof(dbidx) + sizeof(ksiz);
+        offset = 0x00;
+
+        flags = htonl(flags);
+        rnum = htonl(rnum);
+        dbidx = htonl(dbidx);
+        ksiz = htonl(ksiz);
+
+        magicbuf = (char*)malloc(magicbufsize);
+        memset(magicbuf,0,magicbufsize);
+
+        memcpy(magicbuf,&kt_get_magic,sizeof(kt_get_magic));
+        offset=sizeof(kt_get_magic);
+
+        memcpy(magicbuf+offset,&flags,sizeof(flags));
+        offset+=sizeof(flags);
+
+        memcpy(magicbuf+offset,&rnum,sizeof(rnum));
+        offset+=sizeof(rnum);
+
+        memcpy(magicbuf+offset,&dbidx,sizeof(dbidx));
+        offset+=sizeof(dbidx);
+
+        memcpy(magicbuf+offset,&ksiz,sizeof(ksiz));
+
+	if(connect(ktsock, (struct sockaddr *)&sock_in, sizeof(sock_in)) < 0) {
+                return -1;
+        }
+        else {
+                write(ktsock, magicbuf, magicbufsize);
+		tycoon_write(ktsock,gkey);
+		free(magicbuf);	
+	}
 }
 
 int tycoon_remove(unsigned char *rkey) {
@@ -140,6 +153,22 @@ int tycoon_connect(char *thost, char *tport) {
         if(phe = gethostbyname(thost)) memcpy(&sock_in.sin_addr, phe->h_addr, phe->h_length);
         intsock = socket(AF_INET, SOCK_STREAM, 0);
 	return intsock;
+}
+
+tycoon_write(int ktsock,char *data) {
+	int k;
+        offset = 0x00;
+        for( k=0; k<(strlen(data)/BUF_LEN)+1; k++) {
+        	if( strlen(data+offset) < BUF_LEN ) {
+                	memcpy(buf,data+offset,strlen(data+offset));
+                        write(ktsock, buf, strlen(data+offset));
+                }
+                else {
+                        memcpy(buf,data+offset,BUF_LEN);
+                        write(ktsock, buf, BUF_LEN);
+                }
+                offset+=BUF_LEN;
+        }
 }
 
 int tycoon_close(int s) {
